c09113163fccd99e578c58d5099409048e0b559a
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-10-06 11:56:14 -0700

    add core files

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+require('./lib/loadenv.js')();
+var error = require('./lib/error.js');
+var app = require('./lib/app.js');
+var start = require('./lib/startup.js');
+
+start(function (err) {
+  if (err) {
+    error('unable to start', err);
+    process.exit(1);
+  }
+});
+
+module.exports = app;
