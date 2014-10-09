e5babf1af52a4b20849b8ee8fdb2d188d7e894d5
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-10-08 19:23:43 -0700

    add network.js test

--- test/unit/network.js
+++ test/unit/network.js
@@ -3,22 +3,13 @@ var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var network = require('../../lib/models/network.js');
 var redis = require('../../lib/models/redis.js');
+var ip = require('ip');
 
 var createCount = require('callback-count');
 
 lab.experiment('/lib/models/network.js unit test', function () {
   lab.beforeEach(function(done) {
     redis.keys(process.env.WEAVE_NETWORKS+'*', function(err, list) {
-      if (err) { return done(err); }
-      var count = createCount(done);
-      list.forEach(function(item) {
-        redis.del(item, count.inc().next);
-      });
-      count.inc().next();
-    });
-  });
-  lab.beforeEach(function(done) {
-    redis.keys(process.env.WEAVE_ROUTERS+'*', function(err, list) {
       if (err) { return done(err); }
       var count = createCount(done);
       list.forEach(function(item) {
@@ -71,14 +62,188 @@ lab.experiment('/lib/models/network.js unit test', function () {
           if (err) { return done(err); }
           redis.hexists(process.env.WEAVE_NETWORKS, addr, function(err, status) {
             if (err) { return done(err); }
+            Lab.expect(status).to.equal('0');
+            done();
+          });
+        });
+      });
+    });
+    lab.test('remove non allocated addr sould not kill others', function (done) {
+      network.createNetworkAddress('test', function(err, addr) {
+        if (err) { return done(err); }
+
+        Lab.expect(addr).to.equal('10.255.252.0');
+        network.removeNetworkAddress('10.1.1.0', function(err) {
+          if (err) { return done(err); }
+          redis.hexists(process.env.WEAVE_NETWORKS, addr, function(err, status) {
+            if (err) { return done(err); }
             Lab.expect(status).to.equal('1');
             done();
           });
         });
       });
     });
-    lab.test('remove non allocated addr', function (done) {
-      network.removeNetworkAddress('10.255.244.0', done);
-    });
   }); // removeNetworkAddress
+
+  lab.experiment('createHostAddress', function () {
+    var networkIp = '';
+    lab.beforeEach(function(done){
+      network.createNetworkAddress('test', function(err, addr) {
+        if (err) { return done(err); }
+        Lab.expect(addr).to.equal('10.255.252.0');
+        networkIp = addr;
+        done();
+      });
+    });
+    lab.test('invalid param', function (done) {
+      network.createHostAddress('invalid', function(err) {
+        if (err && err.message === 'network not allocated') {
+          return done();
+        }
+        done(new Error('host should not have been created'));
+      });
+    });
+    lab.test('no network', function (done) {
+      network.createHostAddress('10.0.0.0', function(err) {
+        if (err && err.message === 'network not allocated') {
+          return done();
+        }
+        done(new Error('host should not have been created'));
+      });
+    });
+    lab.test('create 1 host from network', function (done) {
+      network.createHostAddress(networkIp, function(err, addr) {
+        if (err) { return done(err); }
+        Lab.expect(addr).to.equal('10.255.252.1');
+        done();
+      });
+    });
+    lab.test('create 2 host from 1 network', function (done) {
+      network.createHostAddress(networkIp, function(err, addr) {
+        if (err) { return done(err); }
+        Lab.expect(addr).to.equal('10.255.252.1');
+        network.createHostAddress(networkIp, function(err, naddr) {
+          if (err) { return done(err); }
+          Lab.expect(naddr).to.equal('10.255.252.2');
+          done();
+        });
+      });
+    });
+    lab.test('create 2 host from 2 network', function (done) {
+      network.createHostAddress(networkIp, function(err, addr) {
+        if (err) { return done(err); }
+        Lab.expect(addr).to.equal('10.255.252.1');
+        network.createNetworkAddress('test', function(err, addr) {
+          if (err) { return done(err); }
+          Lab.expect(addr).to.equal('10.255.248.0');
+          network.createHostAddress(addr, function(err, naddr) {
+            if (err) { return done(err); }
+            Lab.expect(naddr).to.equal('10.255.248.1');
+            done();
+          });
+        });
+      });
+    });
+  }); // createHostAddress
+  lab.experiment('removeHostAddress', function () {
+    var networkIp = '';
+    lab.beforeEach(function(done){
+      network.createNetworkAddress('test', function(err, addr) {
+        if (err) { return done(err); }
+        Lab.expect(addr).to.equal('10.255.252.0');
+        networkIp = addr;
+        done();
+      });
+    });
+
+    lab.test('remove 1 addr', function (done) {
+      network.createHostAddress(networkIp, function(err, addr) {
+        if (err) { return done(err); }
+
+        Lab.expect(addr).to.equal('10.255.252.1');
+        network.removeHostAddress(networkIp, addr, function(err) {
+          if (err) { return done(err); }
+          redis.hexists(process.env.WEAVE_NETWORKS, addr, function(err, status) {
+            if (err) { return done(err); }
+            Lab.expect(status).to.equal('0');
+            done();
+          });
+        });
+      });
+    });
+    lab.test('remove non allocated addr sould not kill others', function (done) {
+      network.createHostAddress(networkIp, function(err, addr) {
+        if (err) { return done(err); }
+
+        Lab.expect(addr).to.equal('10.255.252.1');
+        network.removeHostAddress(networkIp, '10.1.1.0', function(err) {
+          if (err) { return done(err); }
+          redis.hexists(process.env.WEAVE_NETWORKS+':10.255.252.0', '10.255.252.1',
+            function(err, status) {
+              if (err) { return done(err); }
+              Lab.expect(status).to.equal('1');
+              done();
+            });
+        });
+      });
+    });
+  }); // removeHostAddress
+
+  lab.experiment('getRouters', function () {
+    lab.test('list routers', function (done) {
+      network.initRouters(function(err) {
+        if (err) { return done(err); }
+        network.createHostAddress(process.env.WEAVE_ROUTER_NETWORK, function(err, addr1) {
+          if (err) { return done(err); }
+          network.createHostAddress(process.env.WEAVE_ROUTER_NETWORK, function(err, addr2) {
+            if (err) { return done(err); }
+            network.createHostAddress(process.env.WEAVE_ROUTER_NETWORK, function(err, addr3) {
+              if (err) { return done(err); }
+              network.getRouters(function(err, addrs){
+                Lab.expect(addrs).to.contain(process.env.WEAVE_ROUTER_NETWORK);
+                Lab.expect(addrs).to.contain(addr1);
+                Lab.expect(addrs).to.contain(addr2);
+                Lab.expect(addrs).to.contain(addr3);
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+    lab.test('list empty routers', function (done) {
+      network.getRouters(function(err, addrs) {
+        Lab.expect(addrs.length).to.equal(0);
+        done();
+      });
+    });
+  }); // getRouters
+
+  lab.experiment('getRouterMapping', function () {
+    lab.test('get correct mapping', function (done) {
+      network.initRouters(function(err) {
+        if (err) { return done(err); }
+        network.createHostAddress(process.env.WEAVE_ROUTER_NETWORK, function(err, addr) {
+          if (err) { return done(err); }
+          network.getRouterMapping(addr, function(err, host) {
+            Lab.expect(host).to.equal(ip.address());
+            done();
+          });
+        });
+      });
+    });
+  }); // getRouterMapping
+
+  lab.experiment('initRouters', function () {
+    lab.test('list initial router', function (done) {
+      network.initRouters(function(err) {
+        if (err) { return done(err); }
+        network.getRouters(function(err, addrs) {
+          Lab.expect(addrs.length).to.equal(1);
+          Lab.expect(addrs).to.contain(process.env.WEAVE_ROUTER_NETWORK);
+          done();
+        });
+      });
+    });
+  });
 }); //lib/models/network.js unit test
