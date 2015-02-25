5457ab2e3b3990386e2b7d374de53594cc6155b4
anandkumarpatel <anand@runnable.com>
2015-02-25 15:36:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('./lib/loadenv.js')();
 var error = require('./lib/helpers/error.js');
 var app = require('./lib/app.js');
 var start = require('./lib/start.js');
-var error = require('./lib/helpers.js');
 
 start(function (err) {
   if (err) {
