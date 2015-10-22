c17cb4fef113771c38e3d0a77bb68faf801144dd
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-10-21 17:33:39 -0700

    keep removing and staging app

--- test/fixtures/weave-mock.js
+++ test/fixtures/weave-mock.js
@@ -1,18 +1,14 @@
 'use strict';
 var ip = require('ip');
 var docker = require('../../test/fixtures/docker.js').client;
-var networkMap = {};
 
 // checks if weave was launched or not
 var launched = false;
 
 // holds all the mock functions
 var mocks = {
-  setup: mockSetup,
   launch: mockLaunch,
-  attach: mockAttach,
-  detach: mockDetach,
-  status: mockStatus
+  attach: mockAttach
 };
 
 // mocking logic
@@ -28,37 +24,16 @@ var weave = function (cmd, cb) {
   mocks[args[1]].apply(null, args.slice(2));
 };
 
-// mock functions
-function mockSetup (cb) {
-  cb(null, "Pulling repository zettio/weave" +
-  "d125837cefa1: Download complete" +
-  "511136ea3c5a: Download complete" +
-  "f68ba68c4708: Download complete" +
-  "4b67b7bcc993: Download complete" +
-  "1227576fa113: Download complete" +
-  "Status: Image is up to date for zettio/weave:0.8.0" +
-  "Pulling repository zettio/weavedns" +
-  "4e94948cd710: Download complete" +
-  "511136ea3c5a: Download complete" +
-  "f68ba68c4708: Download complete" +
-  "4b67b7bcc993: Download complete" +
-  "8a3e7492759a: Download complete" +
-  "ad14632b52e3: Download complete" +
-  "Status: Image is up to date for zettio/weavedns:0.8.0" +
-  "Pulling repository zettio/weavetools" +
-  "6e956e3a67c6: Download complete" +
-  "511136ea3c5a: Download complete" +
-  "f68ba68c4708: Download complete" +
-  "Status: Image is up to date for zettio/weavetools:0.8.0");
-}
-
 function mockLaunch () {
   var cb = arguments[arguments.length-1];
   if(launched) {
-    return cb("weave is already running.");
+    return cb('weave is already running.');
   }
   launched = true;
-  return cb(null, "e521bb239e333fb9ed77cf5a63700389a068e470bad00eda06dafa9e8332ade5");
+  return cb(null, 'Network 10.0.0.0/8 overlaps with existing route 10.20.128.0/17 on host.
+WARNING: Specified --ipalloc-range 10.0.0.0/8 overlaps with existing route on host.
+Unless this is deliberate, you must pick another range and set it on all hosts.
+8dd84c1f9c3083ee9fd039105de2c5a181d5481f7fdbd45152d801db7cee2c3d');
 }
 
 function mockAttach (cidr, containerId, cb) {
@@ -89,69 +64,27 @@ function mockAttach (cidr, containerId, cb) {
   });
 }
 
-function mockDetach (cidr, containerId, cb) {
-  if (cidr.split('/')[1] > 32) {
-    return cb(new Error('Invalid CIDR: '+cidr+'\n CIDR must of be of form ' +
-      '<ip_address>/<routing_prefix_length>, e.g. 10.0.1.1/24'));
-  }
-  try {
-    ip.cidrSubnet(cidr);
-  } catch(err) {
-    if (err) {
-      return cb(new Error('Failure during network configuration for container ' +
-        containerId + ':\n Error: an inet prefix is expected rather than '+cidr));
-    }
-  }
-  docker.getContainer(containerId).inspect(function(err, data) {
-    if (err) {
-      if (err.statusCode === 404 && err.reason === 'no such container') {
-        return cb(new Error('Error: No such image or container: '+containerId));
-      }
-      return cb(err);
-    }
-    if(!data.State.Running) {
-      return cb(new Error('Container '+containerId+' not running'));
-    }
-    return cb();
-  });
-}
-
 function mockStatus(cb) {
   if (!launched) {
     return cb(new Error('weave container is not present; have you launched it?'));
   }
-  return cb(null, 'Our name is 7a:19:f6:1d:5e:d4 ' +
-    'Sniffing traffic on &{311 65535 ethwe 62:45:cb:53:a1:6a up|broadcast|multicast} ' +
-    'MACs: ' +
-    '62:45:cb:53:a1:6a -> 7a:19:f6:1d:5e:d4 (2014-11-07 22:23:06.092963475 +0000 UTC) ' +
-    '6e:4c:93:e8:ba:e9 -> 7a:19:f6:1d:5e:d4 (2014-11-07 22:23:06.76403584 +0000 UTC) ' +
-    'Peers: ' +
-    'Peer 7a:19:f6:1d:5e:d4 (v0) (UID 7283111983749459475) ' +
-    'Topology: ' +
-    'unicast: ' +
-    '7a:19:f6:1d:5e:d4 -> 00:00:00:00:00:00 ' +
-    'broadcast: ' +
-    '7a:19:f6:1d:5e:d4 -> [] ' +
-    'Reconnects:');
+
+  return cb(null, ['',
+'       Version: 1.1.2',
+'',
+'       Service: router',
+'      Protocol: weave 1..2',
+'          Name: 56:c7:08:af:6b:dd(ip-10-20-219-180)',
+'    Encryption: disabled',
+' PeerDiscovery: enabled',
+'       Targets: 0',
+'   Connections: 0',
+'         Peers: 1',
+'',
+'       Service: ipam',
+'     Consensus: achieved',
+'         Range: [10.0.0.0-11.0.0.0)',
+' DefaultSubnet: 10.0.0.0/8'].join('\n'));
 }
 
 module.exports = weave;
-
-module.exports.set = function(func, mock) {
-  if (!~mocks[func]) {
-    throw new Error('weave does not support cmd: ' + func);
-  }
-  mocks[func] = mock;
-};
-
-module.exports.reset = function(cb) {
-  mocks = {
-    setup: mockSetup,
-    launch: mockLaunch,
-    attach: mockAttach,
-    detach: mockDetach,
-    status: mockStatus
-  };
-  launched = false;
-  cb();
-};
