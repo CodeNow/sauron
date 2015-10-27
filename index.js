0ca455e92a2c2e240a2d4c76d01db7525e1abd04
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-10-26 17:26:18 -0700

    fix index

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ var ErrorCat = require('error-cat');
 var error = new ErrorCat();
 
 Start.startup(function (err) {
-  if (err) {
-    throw error.createAndReport(500, err, 'failed to start');
-  }
+  if (err) { error.createAndReport(500, 'failed to start', err); }
 });
