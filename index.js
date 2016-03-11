fd2ed567f08a5cf0cb364b99e83b68bf3105c997
Anton Podviaznikov <podviaznikov@gmail.com>
2016-03-10 17:15:06 -0800

    feedback from anand

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var Start = require('./lib/start.js');
 var ErrorCat = require('error-cat');
 var error = new ErrorCat();
-var log = require('./lib/logger.js').getChild(__filename)
+var log = require('./lib/logger.js').getChild()
 
 Start.startup(function (err) {
   if (err) {
