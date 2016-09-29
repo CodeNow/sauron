c896ca26cd80caf8fbd51f0b90572a70a597cd9c
Anton Podviaznikov <podviaznikov@gmail.com>
2016-09-28 18:39:31 -0700

    pass org id into weave forget

--- test/unit/workers/weave.peer.forget.js
+++ test/unit/workers/weave.peer.forget.js
@@ -33,62 +33,70 @@ describe('weave.peer.forget.js unit test', function () {
     })
 
     it('should be fine if no errors', function (done) {
+      const orgGitHubId = '123'
       weaveForget({
         dockerHost: '10.0.0.1:4224',
-        hostname: '10.0.0.99'
+        hostname: '10.0.0.99',
+        orgId: orgGitHubId
       })
       .asCallback(function (err) {
         expect(err).to.not.exist()
         sinon.assert.calledOnce(Docker.doesDockExist)
-        sinon.assert.calledWith(Docker.doesDockExist, '10.0.0.1:4224')
+        sinon.assert.calledWith(Docker.doesDockExist, '10.0.0.1:4224', orgGitHubId)
         sinon.assert.calledOnce(WeaveWrapper.forgetAsync)
         sinon.assert.calledWith(WeaveWrapper.forgetAsync, '10.0.0.1:4224', '10.0.0.99')
         done()
       })
     })
     it('should throw error if dock check failed', function (done) {
+      const orgGitHubId = '123'
       const rejectError = new Error('test')
       Docker.doesDockExist.rejects(rejectError)
       weaveForget({
         dockerHost: '10.0.0.1:4224',
-        hostname: '10.0.0.99'
+        hostname: '10.0.0.99',
+        orgId: orgGitHubId
       })
       .asCallback(function (err) {
         expect(err).to.be.instanceOf(Error)
         expect(err).to.equal(rejectError)
         sinon.assert.calledOnce(Docker.doesDockExist)
-        sinon.assert.calledWith(Docker.doesDockExist, '10.0.0.1:4224')
+        sinon.assert.calledWith(Docker.doesDockExist, '10.0.0.1:4224', orgGitHubId)
         sinon.assert.notCalled(WeaveWrapper.forgetAsync)
         done()
       })
     })
     it('should throw fatal error if dock does not exist', function (done) {
+      const orgGitHubId = '123'
       Docker.doesDockExist.resolves(false)
       weaveForget({
         dockerHost: '10.0.0.1:4224',
-        hostname: '10.0.0.99'
+        hostname: '10.0.0.99',
+        orgId: orgGitHubId
       })
       .asCallback(function (err) {
         expect(err).to.be.instanceOf(WorkerStopError)
         expect(err.message).to.equal('Dock was removed')
         sinon.assert.calledOnce(Docker.doesDockExist)
-        sinon.assert.calledWith(Docker.doesDockExist, '10.0.0.1:4224')
+        sinon.assert.calledWith(Docker.doesDockExist, '10.0.0.1:4224', orgGitHubId)
         sinon.assert.notCalled(WeaveWrapper.forgetAsync)
         done()
       })
     })
     it('should throw error if weave command failed', function (done) {
+      const orgGitHubId = '123'
       const rejectError = new Error('test')
       WeaveWrapper.forgetAsync.rejects(rejectError)
       weaveForget({
         dockerHost: '10.0.0.1:4224',
-        hostname: '10.0.0.99'
+        hostname: '10.0.0.99',
+        orgId: orgGitHubId
       })
       .asCallback(function (err) {
         expect(err).to.be.instanceOf(Error)
         expect(err).to.equal(rejectError)
         sinon.assert.calledOnce(Docker.doesDockExist)
-        sinon.assert.calledWith(Docker.doesDockExist, '10.0.0.1:4224')
+        sinon.assert.calledWith(Docker.doesDockExist, '10.0.0.1:4224', orgGitHubId)
         sinon.assert.calledOnce(WeaveWrapper.forgetAsync)
         sinon.assert.calledWith(WeaveWrapper.forgetAsync, '10.0.0.1:4224', '10.0.0.99')
         done()
